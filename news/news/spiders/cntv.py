0001559bc3acbcffd90cfa5711eb2bf3b69a3726
Jason <haojue@gmail.com>
2023-10-20 16:26:17 +0800

    获取当天新闻联播的主要内容

--- news/news/spiders/cntv.py
+++ news/news/spiders/cntv.py
@@ -1,10 +1,32 @@
 import scrapy
-
+import w3lib
+import datetime
+import re
 
 class CntvSpider(scrapy.Spider):
     name = "cntv"
     allowed_domains = ["cntv.cn"]
-    start_urls = ["https://cntv.cn"]
+    # strTime = (datetime.date.today() + datetime.timedelta(-1)).strftime("%Y%m%d")
+    strTime = (datetime.date.today() ).strftime("%Y%m%d")
+    url = f'https://tv.cctv.com/lm/xwlb/day/{strTime}.shtml'
+    start_urls = [url]
 
     def parse(self, response):
-        pass
+
+        url=response.css('li a::attr(href)')[1].get()
+
+        yield scrapy.Request(url, callback=self.parseBrief, dont_filter=True)
+
+
+    def parseBrief(self, response):
+
+
+
+        brief = w3lib.html.remove_tags(response.css('.video_brief').get())
+        lastLine = brief.split('\r\n')[-1]
+        dateStr  = re.findall(r'\d{8}',lastLine)[0]
+        newsDate = datetime.datetime(int(dateStr[0:4]), int(dateStr[4:6]), int(dateStr[6:8]))
+        # 去掉第一行（'本期节目主要内容：）和最后一行（（《新闻联播》 20231018 19:00））的内容
+        brief = '\n'.join(brief.split('\r\n')[1:-1])
+        title = '%s 新闻联播主要内容' % newsDate.strftime("%Y年%m月%d日")
+        yield {'brief':brief,'date':newsDate,'title':title}
